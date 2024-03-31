f6d3928fc9fc6b7dc9560bfc6a153086434d16da
Clarepz <clarence.provenaz@gmail.com>
2024-03-31 22:01:36 +0200

    newPainting

--- fileCreator.sh
+++ fileCreator.sh
@@ -1,10 +1,9 @@
-#! /usr/bin/bash
-# cd content/accueil/expo
-i=0
-for filename in assets/images/CarolePainting/*; do
+#!/bin/zsh
+i=20
+for filename in assets/images/actuel/*; do
     i=$((i+1))
     #echo "${filename##*/}"
-    cd content/accueil/expo
+    cd content/accueil/actuel
     read -r -d '' STR << EOM
     ---
 images:
@@ -17,13 +16,10 @@ tags:
 - accueil
 
 ---
-
-# Markdown
-
 EOM
     echo $i
-    #echo -e "$STR" > expo${filename##*/}.md
-    sed -i 's!# Markdown!!' expo${filename##*/}.md
+    echo -e "$STR" > expo${filename##*/}.md
+    #sed -i 's!# Markdown!!' expo${filename##*/}.md
     cd ..
     cd ..
     cd ..
